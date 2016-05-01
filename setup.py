8188760ac9e6620f6dcfea245d45ee0db35db1dd
Gal Ben David <gal@intsights.com>
2016-05-01 18:55:44 +0300

    zip safe flag has been turned off

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     author='gal@intsights.com',
     author_email='gal@intsights.com',
     description=('A fast, simple, task distribution library'),
-    zip_safe=True,
+    zip_safe=False,
     install_requires=[
         'redis',
         # 'hiredis',
